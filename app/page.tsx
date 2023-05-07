deecdef9826ed44f7c9bdaf24a6e80c1e685fb6a
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-05-07 21:47:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import NameCard from "./components/NameCard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="my-4 left-0 top-0 flex justify-center border-b bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit static w-auto  rounded-xl border  p-4 ">
         This is a card
       </div>
       <NameCard />
 
-      <div className="group rounded-lg border  px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30">
+      <div className="my-4 group rounded-lg border  px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30">
         <h2 className={`mb-3 text-2xl font-semibold`}>Card Heading</h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Card Subheading</p>
       </div>
 
-      <div className="group rounded-lg border  px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30">
+      <div className="mb-4 group rounded-lg border  px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30">
         <h2 className={`mb-3 text-2xl font-semibold`}>Card Heading</h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Card Subheading</p>
       </div>
