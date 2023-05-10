b3b42a03562b875b1c625a37a1eb03e214052625
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-05-10 17:55:51 +0530

    fixed performance issues

--- app/components/CodeBlockCard.tsx
+++ app/components/CodeBlockCard.tsx
@@ -1,5 +1,3 @@
-import { Tilt } from "react-tilt";
-
 interface CodeBlockCardProps {
   // define the props for the CodeBlockCard component here
 }
@@ -13,93 +11,79 @@ declare global {
 }
 
 export default function CodeBlockCard() {
-  const defaultOptions = {
-    reverse: true, // reverse the tilt direction
-    max: 15, // max tilt rotation (degrees)
-    perspective: 1500, // Transform perspective, the lower the more extreme the tilt gets.
-    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-    speed: 1500, // Speed of the enter/exit transition
-    transition: true, // Set a transition on enter/exit.
-    axis: null, // What axis should be disabled. Can be X or Y.
-    reset: true, // If the tilt effect has to be reset on exit.
-    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
-  };
   return (
-    <Tilt options={defaultOptions}>
-      <div className="card group h-full rounded-3xl flex flex-col gap-3 border transition-colors border-neutral-700 bg-neutral-800/30">
-        <div className="w-full h-[30px] bg-[#343830] rounded-t-3xl flex flex-row justify-center items-center gap-3 px-5 ">
-          <div className="flex flex-row gap-1">
-            <div className="rounded-full w-[14px] h-[14px] bg-[#DA2828]"></div>
-            <div className="rounded-full w-[14px] h-[14px] bg-[#F3F800]"></div>
-            <div className="rounded-full w-[14px] h-[14px] bg-[#17F404]"></div>
-          </div>
-
-          <div className="w-full text-center text-base font-mono">
-            Projects.tsx
-          </div>
+    <div className="card group h-full rounded-3xl flex flex-col gap-3 border transition-colors border-neutral-700 bg-neutral-800/30">
+      <div className="w-full h-[30px] bg-[#343830] rounded-t-3xl flex flex-row justify-center items-center gap-3 px-5 ">
+        <div className="flex flex-row gap-1">
+          <div className="rounded-full w-[14px] h-[14px] bg-[#DA2828]"></div>
+          <div className="rounded-full w-[14px] h-[14px] bg-[#F3F800]"></div>
+          <div className="rounded-full w-[14px] h-[14px] bg-[#17F404]"></div>
         </div>
-        <div className="font-mono px-5">
-          <span className="text-red-600">import</span>{" "}
-          <span className="text-cyan-600">React</span>{" "}
-          <span className="text-red-600">from</span>{" "}
-          <span className="text-cyan-600">'react'</span>;
-          <br />
-          <span className="text-red-600">import</span>{" "}
-          <span className="text-cyan-600">ProjObject</span>{" "}
-          <span className="text-red-600">from</span>{" "}
-          <span className="text-cyan-600">'./ProjObject'</span>;
-          <br />
-          <br />
-          <span className="text-red-600">const</span>{" "}
-          <span className="text-cyan-600">Projects</span>{" "}
-          <span className="text-red-600">:</span>{" "}
-          <span className="text-orange-600">React</span>
-          <span>.</span>
-          <span className="text-orange-600">FC</span>{" "}
-          <span className="text-red-600">=</span>{" "}
-          <span className="text-cyan-600">()</span>{" "}
-          <span className="text-red-600">{"=> "}</span>
-          <span className="text-cyan-600">(</span>
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <span className="text-red-600">{"<"}</span>
-          <span className="text-green-600">ProjObject</span>
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span className="text-cyan-600">obj</span>
-          <span className="text-red-600">=</span>{" "}
-          <span className="text-green-600">{"{"}</span>
-          <span className="text-yellow-600">{"{"}</span>
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span>description</span>
-          <span className="text-red-600">:</span>{" "}
-          <span className="text-cyan-600">
-            "I like building projects to learn new frameworks and concepts."
-          </span>
-          <span>,</span>
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span>numberOfProjects</span>{" "}
-          <span className="text-cyan-600"> :</span>{" "}
-          <span className="text-cyan-600">10</span>
-          <span>,</span>
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span className="text-yellow-600">{"}"}</span>
-          <span className="text-green-600">{"}"}</span>
-          <br />
-          &nbsp;&nbsp;
-          <span> {"/>"}</span>
-          <br />
-          <span className="text-cyan-600">)</span>
-          <span>;</span>
-          <br />
-          <br />
-          <span className="text-red-600">export default</span>{" "}
-          <span className="text-cyan-600">Projects</span>
+
+        <div className="w-full text-center text-base font-mono">
+          Projects.tsx
         </div>
       </div>
-    </Tilt>
+      <div className="font-mono px-5">
+        <span className="text-red-600">import</span>{" "}
+        <span className="text-cyan-600">React</span>{" "}
+        <span className="text-red-600">from</span>{" "}
+        <span className="text-cyan-600">'react'</span>;
+        <br />
+        <span className="text-red-600">import</span>{" "}
+        <span className="text-cyan-600">ProjObject</span>{" "}
+        <span className="text-red-600">from</span>{" "}
+        <span className="text-cyan-600">'./ProjObject'</span>;
+        <br />
+        <br />
+        <span className="text-red-600">const</span>{" "}
+        <span className="text-cyan-600">Projects</span>{" "}
+        <span className="text-red-600">:</span>{" "}
+        <span className="text-orange-600">React</span>
+        <span>.</span>
+        <span className="text-orange-600">FC</span>{" "}
+        <span className="text-red-600">=</span>{" "}
+        <span className="text-cyan-600">()</span>{" "}
+        <span className="text-red-600">{"=> "}</span>
+        <span className="text-cyan-600">(</span>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <span className="text-red-600">{"<"}</span>
+        <span className="text-green-600">ProjObject</span>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span className="text-cyan-600">obj</span>
+        <span className="text-red-600">=</span>{" "}
+        <span className="text-green-600">{"{"}</span>
+        <span className="text-yellow-600">{"{"}</span>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span>description</span>
+        <span className="text-red-600">:</span>{" "}
+        <span className="text-cyan-600">
+          "I like building projects to learn new frameworks and concepts."
+        </span>
+        <span>,</span>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span>numberOfProjects</span> <span className="text-cyan-600"> :</span>{" "}
+        <span className="text-cyan-600">10</span>
+        <span>,</span>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span className="text-yellow-600">{"}"}</span>
+        <span className="text-green-600">{"}"}</span>
+        <br />
+        &nbsp;&nbsp;
+        <span> {"/>"}</span>
+        <br />
+        <span className="text-cyan-600">)</span>
+        <span>;</span>
+        <br />
+        <br />
+        <span className="text-red-600">export default</span>{" "}
+        <span className="text-cyan-600">Projects</span>
+      </div>
+    </div>
   );
 }
--- app/components/ExperienceCard.tsx
+++ app/components/ExperienceCard.tsx
@@ -1,4 +1,3 @@
-import { Tilt } from "react-tilt";
 import styles from "../styles/ExperienceCard.module.css";
 interface ExperienceCardProps {}
 
@@ -10,116 +9,103 @@ declare global {
   }
 }
 export default function ExperienceCard() {
-  const defaultOptions = {
-    reverse: true, // reverse the tilt direction
-    max: 15, // max tilt rotation (degrees)
-    perspective: 1500, // Transform perspective, the lower the more extreme the tilt gets.
-    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-    speed: 1500, // Speed of the enter/exit transition
-    transition: true, // Set a transition on enter/exit.
-    axis: null, // What axis should be disabled. Can be X or Y.
-    reset: true, // If the tilt effect has to be reset on exit.
-    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
-  };
   return (
-    <Tilt options={defaultOptions}>
-      <div
-        className={
-          "card group rounded-3xl w-auto flex flex-col h-full items-center justify-center  border  px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30"
-        }
+    <div
+      className={
+        "card group rounded-3xl w-auto flex flex-col h-full items-center justify-center  border  px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30"
+      }
+    >
+      <h2
+        className={`mb-3 text-3xl md:text-5xl text-center md:text-start font-semibold`}
+        // make the outline appear
       >
-        <h2
-          className={`mb-3 text-3xl md:text-5xl text-center md:text-start font-semibold`}
-          // make the outline appear
-        >
-          Work Experience
-        </h2>
-        <ul className={styles.timeline}>
-          <li>
-            <div className={styles["direction-r"]}>
-              <div className={styles["flag-wrapper"]}>
-                <span className={styles["flag"]}>Adewunmi Skincare</span>
-                <span className={styles["time-wrapper"]}>
-                  <span className={styles["time"]}>May 2023 - Present</span>
-                </span>
-              </div>
-              <div className={styles.desc}>
-                <p>
-                  <b>Frontend Developer Intern</b>
-                </p>
-                <br />
-                <p>
-                  ● Designed and Developed various web pages using Figma, MUI
-                  and React
-                </p>
-                <br />
-              </div>
+        Work Experience
+      </h2>
+      <ul className={styles.timeline}>
+        <li>
+          <div className={styles["direction-r"]}>
+            <div className={styles["flag-wrapper"]}>
+              <span className={styles["flag"]}>Adewunmi Skincare</span>
+              <span className={styles["time-wrapper"]}>
+                <span className={styles["time"]}>May 2023 - Present</span>
+              </span>
             </div>
-          </li>
+            <div className={styles.desc}>
+              <p>
+                <b>Frontend Developer Intern</b>
+              </p>
+              <br />
+              <p>
+                ● Designed and Developed various web pages using Figma, MUI and
+                React
+              </p>
+              <br />
+            </div>
+          </div>
+        </li>
 
-          <li>
-            <div className={styles["direction-l"]}>
-              <div className={styles["flag-wrapper"]}>
-                <span className={styles.flag}>GreyJay Energy.</span>
-                <span className={styles["time-wrapper"]}>
-                  <span className={styles.time}>Jun 2022 - Aug 2022</span>
-                </span>
-              </div>
-              <div className={styles.desc}>
-                <p>
-                  <b>Software Developer Intern</b>
-                </p>
-                <br />
-                <p>
-                  ● Developed an energy usage visualization app for Android that
-                  reduced energy consumption by end-users.
-                </p>
-                <br />
-                <p>
-                  ● Enhanced app functionality and user experience by creating
-                  customized Vue.js and Javascript widgets.
-                </p>{" "}
-                <br />
-                <p>
-                  ● Integrated push notifications with Google Firebase to ensure
-                  timely updates and alerts.
-                </p>
-                <br />
-                <p>
-                  ● Conducted log-linear analysis using Python's Scikit-Learn
-                  library and created UI chart components using Vue.js and
-                  ChartJs.
-                </p>
-              </div>
+        <li>
+          <div className={styles["direction-l"]}>
+            <div className={styles["flag-wrapper"]}>
+              <span className={styles.flag}>GreyJay Energy.</span>
+              <span className={styles["time-wrapper"]}>
+                <span className={styles.time}>Jun 2022 - Aug 2022</span>
+              </span>
             </div>
-          </li>
+            <div className={styles.desc}>
+              <p>
+                <b>Software Developer Intern</b>
+              </p>
+              <br />
+              <p>
+                ● Developed an energy usage visualization app for Android that
+                reduced energy consumption by end-users.
+              </p>
+              <br />
+              <p>
+                ● Enhanced app functionality and user experience by creating
+                customized Vue.js and Javascript widgets.
+              </p>{" "}
+              <br />
+              <p>
+                ● Integrated push notifications with Google Firebase to ensure
+                timely updates and alerts.
+              </p>
+              <br />
+              <p>
+                ● Conducted log-linear analysis using Python's Scikit-Learn
+                library and created UI chart components using Vue.js and
+                ChartJs.
+              </p>
+            </div>
+          </div>
+        </li>
 
-          <li>
-            <div className={styles["direction-r"]}>
-              <div className={styles["flag-wrapper"]}>
-                <span className={styles["flag"]}>Better Drones</span>
-                <span className={styles["time-wrapper"]}>
-                  <span className={styles["time"]}>Oct 2018 - Jan 2019</span>
-                </span>
-              </div>
-              <div className={styles["desc"]}>
-                <p>
-                  <b>Engineering Intern</b>
-                </p>
-                <p>
-                  ● Recommended solutions to clients based on time and resource
-                  constraints.
-                </p>
-                <br />
-                <p>
-                  ● Created project reports and quotes tailored to client
-                  requirements and budget.
-                </p>
-              </div>
+        <li>
+          <div className={styles["direction-r"]}>
+            <div className={styles["flag-wrapper"]}>
+              <span className={styles["flag"]}>Better Drones</span>
+              <span className={styles["time-wrapper"]}>
+                <span className={styles["time"]}>Oct 2018 - Jan 2019</span>
+              </span>
             </div>
-          </li>
-        </ul>
-      </div>
-    </Tilt>
+            <div className={styles["desc"]}>
+              <p>
+                <b>Engineering Intern</b>
+              </p>
+              <p>
+                ● Recommended solutions to clients based on time and resource
+                constraints.
+              </p>
+              <br />
+              <p>
+                ● Created project reports and quotes tailored to client
+                requirements and budget.
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
