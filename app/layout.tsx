3bd4f7ececa69bd4f2ebe615230f534865455e98
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-05-11 22:33:44 +0530

    added msft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "gzo6f1slbt");`,
+        }}
+      ></Script>
       <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
