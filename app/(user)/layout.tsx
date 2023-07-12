d1167ca555104ef706e31d2588f289712911db6a
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-07-12 18:16:17 +0530

    fixed layout

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,6 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "gzo6f1slbt");`,
+        }}
+      ></Script>
+      <Analytics />
       <body className={inter.className}>
         {/* Give Grid Background */}
         <div className="main " />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,17 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
-(function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "gzo6f1slbt");`,
-        }}
-      ></Script>
-      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
