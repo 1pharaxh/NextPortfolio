b94e7663d8765af5c7bda6d5c8137b74d356801e
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-07-25 17:35:21 +0530

    fixed revalidation

--- pages/api/sanityWebhook.ts
+++ pages/api/sanityWebhook.ts
@@ -14,7 +14,7 @@ export default async function handler(
   }
 
   if (req.headers.authorization !== secret) {
-    // console.log(req.headers.authorization, secret);
+    console.log(req.headers.authorization, secret);
     console.error("Invalid secret");
     return res.status(401).json({ message: "Invalid secret" });
   }
@@ -22,15 +22,17 @@ export default async function handler(
   try {
     // take 5 seconds to revalidate
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    await res.revalidate(`/blog`);
     await res.revalidate(`/`);
+    await res.revalidate(`/`);
+    await res.revalidate(`/blog`);
+    await res.revalidate(`/blog`);
 
     console.log("Revalidated");
     return res.json({
-      message: `Revalidated "/", "/blog" and Robots`,
+      message: `Revalidated "/" and "/blog"`,
     });
   } catch (err) {
-    console.log(err);
-    return res.status(500).send({ message: `Error revalidating ${err}` });
+    console.error(err);
+    return res.status(500).send({ message: `Error: ${err}` });
   }
 }
